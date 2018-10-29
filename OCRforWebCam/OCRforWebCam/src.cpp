38bc5c4591f71eda9112bd07938859ec14ac4756
elemmire <351059171@qq.com>
2018-10-29 21:00:23 +0800

    遍历文件

--- OCRforWebCam/OCRforWebCam/src.cpp
+++ OCRforWebCam/OCRforWebCam/src.cpp
@@ -7,16 +7,50 @@
 #include <opencv2/highgui/highgui.hpp>
 
 #include <stdio.h>
-#include <sys/types.h>
-#include <direct.h>
-
-//#include <dir>
-
+#include <io.h>
+#include <string.h>
 
 using namespace cv;
 using namespace std;
 
+void  listFiles(const char*);
+
 int main(int argc, char** argv)
 {
+	char dir[200];
+	cout << "Enter a directory (ends with \'\\\'): ";
+	cin.getline(dir, 200);
 
+	strcat_s(dir, "*.jpg");        // 在要遍历的目录后加上通配符
+	listFiles(dir);
+
+	return 0;
+	
+}
+
+void listFiles(const char * dir)
+{
+	intptr_t handle;
+	_finddata_t findData;
+
+	handle = _findfirst(dir, &findData);    // 查找目录中的第一个文件
+	if (handle == -1)
+	{
+		cout << "Failed to find first file!\n";
+		return;
+	}
+
+	do
+	{
+		if (findData.attrib & _A_SUBDIR
+			&& strcmp(findData.name, ".") == 0
+			&& strcmp(findData.name, "..") == 0
+			)    // 是否是子目录并且不为"."或".."
+			cout << findData.name << "\t<dir>\n";
+		else
+			cout << findData.name << "\t" << findData.size << endl;
+	} while (_findnext(handle, &findData) == 0);    // 查找目录中的下一个文件
+
+	cout << "Done!\n";
+	_findclose(handle);    // 关闭搜索句柄
 }
