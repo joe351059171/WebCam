3085f61daec1d76f891a19b8a97b4b07d41be99d
elemmire <351059171@qq.com>
2018-11-06 21:41:04 +0800

    commit

--- OCRforWebCam/OCRforWebCam/src.cpp
+++ OCRforWebCam/OCRforWebCam/src.cpp
@@ -101,7 +101,7 @@ void FileScan(const char * dirwithformat,const char* dir)
 	intptr_t handle;
 	_finddata_t findData;
 
-	handle = _findfirst(dirwithformat, &findData);    // 查找目录中的第一个文件
+	handle = _findfirst(dirwithformat, &findData);    
 	if (handle == -1)
 	{
 		cout << "Failed to find first file!\n";
@@ -113,7 +113,7 @@ void FileScan(const char * dirwithformat,const char* dir)
 		if (findData.attrib & _A_SUBDIR
 			&& strcmp(findData.name, ".") == 0
 			&& strcmp(findData.name, "..") == 0
-			)    // 是否是子目录并且不为"."或".."
+			)
 			cout << findData.name << "\t<dir>\n";
 		else {
 			cout << findData.name << "\t" << findData.size << endl;
@@ -121,10 +121,10 @@ void FileScan(const char * dirwithformat,const char* dir)
 			fprintf(input, "%s%s\n",dir,findData.name);
 			fclose(input);
 		}
-	} while (_findnext(handle, &findData) == 0);    // 查找目录中的下一个文件
+	} while (_findnext(handle, &findData) == 0);    
 
 	cout << "Done!\n";
-	_findclose(handle);    // 关闭搜索句柄
+	_findclose(handle);
 }
 
 /*void imgproc(const char* filename,int thresh) {
