d17b959d41dbbd5ab0a67dc68363f33ec0e479f4
elemmire <351059171@qq.com>
2018-11-06 11:28:28 +0800

    .

--- OCRforWebCam/OCRforWebCam/src.cpp
+++ OCRforWebCam/OCRforWebCam/src.cpp
@@ -76,7 +76,7 @@ int main(int argc, char** argv)
 	Mat response, tmp;
 	tmp = response_array.reshape(1, 1); //make continuous
 	tmp.convertTo(response, CV_32FC1); // Convert  to float
-	sample = sample.reshape(1, 1);
+//	sample = sample.reshape(1, 1);
 	sample.convertTo(sample, CV_32FC1);
 	FileStorage Data("TrainingData.yml", FileStorage::WRITE); // !!!!!!!!!!!!!!!!!!!!!!!!
 	Data << "data" << sample;
