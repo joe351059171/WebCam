fec0dbac00a0fc8c6cfa183c057afaeed34d8b19
elemmire <351059171@qq.com>
2019-02-23 12:19:12 +0800

    change

--- OCRforWebCam/OCRforWebCam/src.cpp
+++ OCRforWebCam/OCRforWebCam/src.cpp
@@ -15,10 +15,11 @@ using namespace cv;
 using namespace std;
 
 void FileScan(const char*,const char*);
-//void imgproc(const char*,int, Mat, Mat);
 
 int main(int argc, char** argv)
 {
+	if (argc == 1)
+		printf("usage:OCR.exe \"picture path ends with \'\\\'\" threshold value(if necessary) \n");
 	Mat sample;
 	Mat response_array;
 	char dir[200] = { 0,0,0,0, };
@@ -28,8 +29,6 @@ int main(int argc, char** argv)
 	int the = 100;
 	if(argc>2)
 	the	= atoi(argv[2]);
-	if (argc==0)
-		printf("usage:.OCR.exe \"picture path\ends with a \'\\\'\" threshold value(if necessary) \n");
 	//cin.getline(dir, 200);
 	strcpy(dirwithformat, dir);
 	strcat_s(dirwithformat, "*.jpg");
@@ -131,42 +130,3 @@ void FileScan(const char * dirwithformat,const char* dir)
 	cout << "Done!\n";
 	_findclose(handle);
 }
-
-/*void imgproc(const char* filename,int thresh) {
-	//Process image to extract contour
-	Mat thr, gray, con;
-	Mat src = imread(filename, 1);
-	//if(file.cols*file.cols< 484000)
-	//	resize(file, src,Size(),8,8, INTER_CUBIC);
-	imshow("file",src);
-	cvtColor(src, gray, CV_BGR2GRAY);
-	threshold(gray, thr, thresh, 255, THRESH_BINARY_INV); //Threshold to find contour
-	imshow("thr", thr);
-	thr.copyTo(con);
-
-	// Create sample and label data
-	vector< vector <Point> > contours; // Vector for storing contour
-	vector< Vec4i > hierarchy;
-
-	findContours(con, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE); //Find contour
-	
-	for (int i = 0; i < contours.size(); i = hierarchy[i][0]) // iterate through first hierarchy level contours
-	{
-			Rect r = boundingRect(contours[i]); //Find bounding rect for each contour
-			rectangle(src, Point(r.x, r.y), Point(r.x + r.width, r.y + r.height), Scalar(0, 0, 255), 2, 8, 0);
-			Mat ROI = thr(r); //Crop the image
-			Mat tmp1, tmp2;
-			resize(ROI, tmp1, Size(10, 10), 0, 0, INTER_LINEAR); //resize to 10X10
-			tmp1.convertTo(tmp2, CV_32FC1); //convert to float
-			sample.push_back(tmp2.reshape(1, 1)); // Store  sample data
-			imshow("src", src);
-			int c = waitKey(0); // Read corresponding label for contour from keyoard
-			c -= 0x30;     // Convert ascii to intiger value
-			if (c >= 0 && c <= 9) {
-				response_array.push_back(c); // Store label to a mat
-				rectangle(src, Point(r.x, r.y), Point(r.x + r.width, r.y + r.height), Scalar(0, 255, 0), 2, 8, 0);
-			}
-	}
-	imshow("src", src);
-	waitKey(0);
-}*/
